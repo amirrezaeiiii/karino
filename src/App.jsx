49550f9711139ffe688ac62bffdb05cbdb7a0d7e
amir <a.rezaei1090@gmail.com>
2026-07-14 01:43:15 +0330

    Add freelancer dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { DarkModeProvier } from "./context/DarkModeContext";
+import FreelancerLayout from "./features/freelancer/FreelancerLayout";
+import OwnerLayout from "./features/owner/OwnerLayout";
 import Auth from "./pages/Auth";
 import CompleteProfile from "./pages/CompleteProfile";
+import FreelancerDashboard from "./pages/FreelancerDashboard";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import OwnerDashboard from "./pages/OwnerDashboard";
 import Project from "./pages/Project";
 import Projects from "./pages/Projects";
-import AppLayout from "./ui/AppLayout";
-import OwnerLayout from "./features/owner/OwnerLayout";
-import DashboardLayout from "./features/owner/DashboardLayout";
+import Proposals from "./pages/Proposals";
+import SubmittedProjects from "./pages/SubmittedProjects";
 
 const queryClient = new QueryClient();
 
@@ -25,9 +27,15 @@ function App() {
           <Route path="/complete-profile" element={<CompleteProfile />} />
           <Route path="/owner" element={<OwnerLayout />}>
             <Route index element={<Navigate to="dashboard" />} />
-            <Route path="dashboard" element={<DashboardLayout />} />
+            <Route path="dashboard" element={<OwnerDashboard />} />
             <Route path="projects" element={<Projects />} />
             <Route path="projects/:id" element={<Project />} />
+          </Route>
+          <Route path="/freelancer" element={<FreelancerLayout />}>
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route path="dashboard" element={<FreelancerDashboard />} />
+            <Route path="proposals" element={<Proposals />} />
+            <Route path="projects" element={<SubmittedProjects />} />
           </Route>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
---
+++ src/features/freelancer/FreelancerLayout.jsx
@@ -0,0 +1,35 @@
+import { HiOutlineBriefcase, HiOutlineCog6Tooth, HiOutlineDocument, HiOutlineFolder, HiOutlineHome } from "react-icons/hi2";
+import AppLayout from "../../ui/AppLayout";
+import CustomNavLink from "../../ui/CustomNavLink";
+import Sidebar from "../../ui/Sidebar";
+
+function FreelancerLayout() {
+  return (
+    <AppLayout>
+      <Sidebar>
+        <CustomNavLink to="/">
+          <HiOutlineHome />
+          <span>صفحه اصلی</span>
+        </CustomNavLink>
+        <CustomNavLink to="dashboard">
+          <HiOutlineBriefcase />
+          <span>داشبورد</span>
+        </CustomNavLink>
+        <CustomNavLink to="proposals">
+          <HiOutlineFolder />
+          <span>پروژه ها</span>
+        </CustomNavLink>
+          <CustomNavLink to="projects">
+         <HiOutlineDocument />
+          <span>درخواست ها</span>
+        </CustomNavLink>
+        <CustomNavLink to="setting">
+          <HiOutlineCog6Tooth />
+          <span>تنظیمات</span>
+        </CustomNavLink>
+      </Sidebar>
+    </AppLayout>
+  );
+}
+
+export default FreelancerLayout;
